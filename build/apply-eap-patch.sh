01786249dd6388b7e42127280fac9fcc770ffbc6
abinabraham23 <abinabraham@ymail.com>
2020-12-01 17:27:40 +0100

    Updated the Dockerfile and related scripts

---
+++ build/apply-eap-patch.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -e
+
+function logMessage ()
+{
+    echo ""
+    printf "$1${NC}"
+    echo ""
+}
+
+function applyPatch ()
+{
+    logMessage "Applying patch";
+    $JBOSS_HOME/bin/jboss-cli.sh "patch apply /tmp/${PATCH_ARTIFACT}";
+    logMessage "Patch applied successfully";
+}
+
+function cleanup ()
+{
+    rm -rf /tmp/patch.sh /tmp/{PATCH_ARTIFACT}
+    logMessage "Temp files deleted..";   
+}
+
+# Steps
+
+applyPatch
+cleanup
